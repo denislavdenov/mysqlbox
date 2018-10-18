40f5ecc122215eeddf813b88a1336f5dd1a6cfbd
Denislav Denov <denislavdenov@Denislavs-MacBook-Pro.local>
2018-10-18 14:25:46 +0300

    adding more tests

--- box/test/integration/default/check_pkg.rb
+++ box/test/integration/default/check_pkg.rb
@@ -2,3 +2,16 @@ describe package('mysql-server') do
   it { should be_installed }
 end
 
+describe service('mysql') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe mysql_conf do
+  its('mysqld.port') { should cmp 3306 }
+end
+
+describe mysql_session(' root',' vagrant').query('SELECT * FROM *') do
+  its('exit_status') { should eq(1) }
+end
